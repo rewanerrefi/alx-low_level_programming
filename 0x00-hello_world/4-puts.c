68bfd34ccb6b145d349e112253eafd2ac9ae5363
rewanerrefi <rewanmohamed1812@gmail.com>
2023-06-15 17:02:07 -0700

    rrr

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
-int main(void){
-	puts("/"Programming is like building a multilingual puzzle");
+/**
+ * main - Entry point
+ *
+ * Description: A C program that print with put function
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
 		return(0);
 }	
