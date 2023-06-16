0edb365b2f6a17b3d728df35d768d9f9c00b59e5
rewanerrefi <rewanmohamed1812@gmail.com>
2023-06-15 19:32:32 -0700

    hhh

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,6 @@
 
 int main(void)
 {
-	
 	printf("Size of a char: %u  byte(s)\n", sizeof(char));
 	printf("Size of an int: %u  byte(s)\n", sizeof(int));
 	printf("Size of a long int: %u  byte(s)\n", sizeof(long int));
