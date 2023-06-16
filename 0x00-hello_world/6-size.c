df48672bf668c24329362e133d501bbb814a2583
rewanerrefi <rewanmohamed1812@gmail.com>
2023-06-15 19:04:12 -0700

    ddd

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,11 +10,11 @@
 
 int main(void)
 {
-	printf("Size of char pointer: %d\n", sizeof(char));
-	printf("\nSize of int pointer: %d\n", sizeof(int));
-	printf("\nSize of float pointer: %d\n", sizeof(float));
-	printf("\nSize of long int pointer: %d\n", sizeof(long int));
-	printf("\nSize of double pointer: %d\n", sizeof(double));
+	printf("Size of char pointer: %lu\n", sizeof(char));
+	printf("\nSize of int pointer: %lu\n", sizeof(int));
+	printf("\nSize of float pointer: %lu\n", sizeof(float));
+	printf("\nSize of long int pointer: %lu\n", sizeof(long int));
+	printf("\nSize of double pointer: %lu\n", sizeof(double));
 	return (0);
 }
 
