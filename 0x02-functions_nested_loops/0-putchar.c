85dfc2e2cfd0b75e44d15648175f81cd3eac55ac
rewanerrefi <rewanmohamed1812@gmail.com>
2023-06-20 16:02:49 -0700

    ddd

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,12 +7,12 @@
 
 int main(void)
 {
-char c[9] = "_putchar";
-int i = 0;
-while (i < 8)
+	char c[9] = "_putchar";
+	int i = 0
+	while (i < 8)
 {
-_putchar(c[i]);
-i++;
+	_putchar(c[i]);
+	i++;
 }
 _putchar('\n');
 return (0);
